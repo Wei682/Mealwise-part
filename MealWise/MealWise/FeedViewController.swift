ac736c8d25e2abc2ee87fce0a17b172259f90cac
Wei <xiawei29@hotmail.com>
2022-04-07 14:03:10 -0400

    Setup, register, and login

---
+++ MealWise/MealWise/FeedViewController.swift
@@ -0,0 +1,41 @@
+//
+//  FeedViewController.swift
+//  MealWise
+//
+//  Created by Wei Xia on 4/7/22.
+//
+
+import UIKit
+import Parse
+
+class FeedViewController: UIViewController {
+
+    @IBAction func onLogOut(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(identifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else { return }
+        
+        delegate.window?.rootViewController = loginViewController
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
